a76c4f9f2857b10a60fb0f853a637ea40f157ac4
Joseph Patton <josephpatton@outlook.com>
2024-05-24 21:37:27 +0100

    refactor: make backend a library

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
+use backend::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(health_check))
-            .route("/{name}", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
