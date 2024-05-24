ce22c442c6c870910724ff9178d9e6e109ab18c0
Joseph Patton <josephpatton@outlook.com>
2024-05-24 20:26:39 +0100

    feat: replace greet request handler with health_check

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
