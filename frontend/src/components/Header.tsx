c466c7577793ff5a4484e201ccd36c247e2eef3d
Joseph Patton <josephpatton@outlook.com>
2024-05-29 09:28:49 +0100

    remake frontend with vanilla js react app

--- frontend/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Header() {
-    return (
-        <div className="header">
-            <h1 className="notes__title">Notes App</h1>
-        </div>
-    );
-}
-export default Header;
--- frontend/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import "../components/css/app.css";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
