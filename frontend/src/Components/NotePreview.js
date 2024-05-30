7cef4122a9b729c7c948c6c87d3745434380c745
Joseph Patton <josephpatton@outlook.com>
2024-05-30 17:33:35 +0100

    feat: limit titles in a NotePreview to single line using elipses

--- frontend/src/Components/NotePreview.js
+++ frontend/src/Components/NotePreview.js
@@ -3,9 +3,9 @@ import "./css/NoteList.css";
 function NotePreview({ id, text, title, tag, deleteNote }) {
   return (
     <div className="note-preview box">
-      <div className="title box">{title}</div>
-      <div className="text box">{text}</div>
-      <div className="tag box">{tag}</div>
+      <div className="title overflow">{title}</div>
+      <div className="text">{text}</div>
+      <div className="tag">{tag}</div>
     </div>
   );
 }
