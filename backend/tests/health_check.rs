1b856e818aaf89485ffeb9663bac90676ad67589
Joseph Patton <josephpatton@outlook.com>
2024-05-31 15:56:43 +0100

    test: add tests for 400 response for malformed create_node HTTP request body

--- backend/tests/health_check.rs
+++ backend/tests/health_check.rs
@@ -38,7 +38,7 @@ async fn create_note_returns_a_200_for_valid_data() {
         "id": 1,
         "title": "Test Note",
         "content": "test note content",
-        "tag": "test"
+        "tag": "test",
     })
     .to_string();
 
@@ -53,4 +53,49 @@ async fn create_note_returns_a_200_for_valid_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn create_note_returns_a_400_when_data_is_missing() {
+    // Arrange
+    // TODO: systematic way to generate failing test cases
+    let note_body_missing_id = json!({
+        "title": "Test Note",
+        "content": "test note content",
+        "tag": "test",
+    })
+    .to_string();
+
+    let note_body_missing_title = json!({
+        "id": 1,
+        "content": "test note content",
+        "tag": "test",
+    })
+    .to_string();
+
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        (note_body_missing_id, "missing the id"),
+        (note_body_missing_title, "missing the title"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/notes", &app_address))
+            .header("Content-Type", "application/json")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
