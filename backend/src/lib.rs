3a41ff12bf8107012aac5ca84769beec62e8e78b
Joseph Patton <josephpatton@outlook.com>
2024-05-31 13:01:45 +0100

    feat: add skeleton create_test handler and api endpoint

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -7,9 +7,17 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn create_note() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/notes", web::post().to(create_note))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
