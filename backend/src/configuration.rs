0cecbb97371e4656f9f8e52d9f4d6ffc274eb269
Joseph Patton <josephpatton@outlook.com>
2024-06-01 14:33:24 +0100

    fix configuration path used in get_configuration

--- backend/src/configuration.rs
+++ backend/src/configuration.rs
@@ -14,11 +14,22 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
-
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
     // Initialise our configuration reader
     let settings = config::Config::builder()
-        .add_source(config::File::new("configuration", FileFormat::Yaml))
+        .add_source(config::File::from(
+            configuration_directory.join("configuration.yaml"),
+        ))
         .build()?;
     settings.try_deserialize::<Settings>()
 }
