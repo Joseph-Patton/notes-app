746b25829623515914a547773574c089ef610b91
Joseph Patton <josephpatton@outlook.com>
2024-06-01 16:28:57 +0100

    feat:  pass postgres connection to HttpServer

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,16 @@
 use backend::configuration::get_configuration;
 use backend::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,13 +1,16 @@
 use crate::routes::{create_note, health_check};
 use actix_web::dev::{self, Server};
 use actix_web::{web, App, HttpResponse, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/notes", web::post().to(create_note))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
