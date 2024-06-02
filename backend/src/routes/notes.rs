3ca1347350f1c25fba3d213e4329ef9c5fb2fbb1
Joseph Patton <josephpatton@outlook.com>
2024-06-02 16:31:08 +0100

    feat: use span for tracing

--- backend/src/routes/notes.rs
+++ backend/src/routes/notes.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 #[derive(serde::Deserialize, Debug)]
 pub struct Note {
@@ -11,11 +12,13 @@ pub struct Note {
 
 pub async fn create_note(note: web::Json<Note>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id{} - Adding a new note '{}'.",
-        request_id,
-        note.title, //remove this after testing, user notes should not be logged
+    let request_span = tracing::info_span!(
+        "Adding a new note.",
+        %request_id,
+        note_title = %note.title,
     );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO notes (id, title, content, tag, created_at)
@@ -28,18 +31,12 @@ pub async fn create_note(note: web::Json<Note>, pool: web::Data<PgPool>) -> Http
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("request_id {} - New note have been created", request_id);
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
