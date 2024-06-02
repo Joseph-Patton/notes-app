8247123c8e5c3c9c8eb8852488518d7ecee65222
Joseph Patton <josephpatton@outlook.com>
2024-06-02 16:54:57 +0100

    feat: output tracing messages

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,21 @@
 use backend::configuration::get_configuration;
 use backend::startup::run;
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::{subscriber::set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //tracing
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("backend".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
     //configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
