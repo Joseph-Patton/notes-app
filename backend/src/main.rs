567d03e6d077cf70544d2f284e4dd18cc58df749
Joseph Patton <josephpatton@outlook.com>
2024-06-12 13:12:39 +0100

    refactor: move logic for creation of application from settings to startup.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,6 @@
 use backend::configuration::get_configuration;
-use backend::startup::run;
+use backend::startup::build;
 use backend::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,15 +9,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     //configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,10 +1,26 @@
+use crate::configuration::Settings;
 use crate::routes::{create_note, health_check};
 use actix_cors::Cors;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)
+}
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
