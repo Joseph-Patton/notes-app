79769426afd11deb09ba9d26ca15e39d6dd3695a
Joseph Patton <josephpatton@outlook.com>
2024-06-12 12:07:23 +0100

    enable CORS for backend

--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,4 +1,5 @@
 use crate::routes::{create_note, health_check};
+use actix_cors::Cors;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
@@ -9,6 +10,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // TODO change cors for production
+            .wrap(Cors::permissive())
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/notes", web::post().to(create_note))
