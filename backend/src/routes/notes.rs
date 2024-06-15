2c525ee89ff95043cf6c8965d92bb5c2b182f906
Joseph Patton <josephpatton@outlook.com>
2024-06-15 15:13:03 +0100

    feat: add get respone for /notes endpoint

--- backend/src/routes/notes.rs
+++ backend/src/routes/notes.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, HttpResponse};
-use chrono::Utc;
+use actix_web::{http::header::ContentType, web, HttpResponse};
+use chrono::{serde::ts_seconds, DateTime, Utc};
 use sqlx::PgPool;
 use uuid::Uuid;
+
 #[derive(serde::Deserialize, Debug)]
-pub struct Note {
+pub struct UninitialisedNote {
     title: String,
     content: String,
     tag: String,
@@ -12,30 +13,36 @@ pub struct Note {
 //web logic
 #[tracing::instrument(
 name = "Adding a new note",
-skip(note, pool),
+skip(uniitialised_note, pool),
 fields(
-note_title = %note.title // TODO remove eventually so that user notes are not logged, keep for testing
+note_title = %uniitialised_note.title // TODO remove eventually so that user notes are not logged
 )
 )]
-pub async fn create_note(note: web::Json<Note>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_note(&pool, &note).await {
+pub async fn create_note(
+    uniitialised_note: web::Json<UninitialisedNote>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match insert_note(&pool, &uniitialised_note).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 //database logic
-#[tracing::instrument(name = "Saving new note in the database", skip(note, pool))]
-pub async fn insert_note(pool: &PgPool, note: &Note) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving new note in the database", skip(unitialised_note, pool))]
+pub async fn insert_note(
+    pool: &PgPool,
+    unitialised_note: &UninitialisedNote,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO notes (id, title, content, tag, created_at)
         VALUES ($1, $2, $3, $4, $5)
         "#,
         Uuid::new_v4(),
-        note.title,
-        note.content,
-        note.tag,
+        unitialised_note.title,
+        unitialised_note.content,
+        unitialised_note.tag,
         Utc::now()
     )
     .execute(pool)
@@ -45,4 +52,44 @@ pub async fn insert_note(pool: &PgPool, note: &Note) -> Result<(), sqlx::Error> 
         e
     })?;
     Ok(())
+}
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+pub struct Note {
+    pub id: Uuid,
+    pub title: String,
+    pub content: Option<String>,
+    pub tag: Option<String>,
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+pub type Notes = Vec<Note>;
+
+#[tracing::instrument(name = "Getting notes list", skip(pool))]
+pub async fn return_notes(pool: web::Data<PgPool>) -> HttpResponse {
+    match fetch_notes(&pool).await {
+        Ok(notes) => HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(serde_json::to_string(&notes).unwrap()),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+//database logic
+#[tracing::instrument(name = "Fetching notes list from database", skip(pool))]
+pub async fn fetch_notes(pool: &PgPool) -> Result<Notes, sqlx::Error> {
+    let notes = sqlx::query_as!(
+        Note,
+        r#"
+        SELECT id, title, content, tag, created_at FROM notes
+        "#
+    )
+    .fetch_all(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    //info_span!("Content of notes: {:?}", notes);
+    Ok(notes)
 }
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
-use crate::routes::{create_note, health_check};
+use crate::routes::{create_note, health_check, return_notes};
 use actix_cors::Cors;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
@@ -50,6 +50,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/notes", web::post().to(create_note))
+            .route("/notes", web::get().to(return_notes))
             .app_data(db_pool.clone())
     })
     .listen(listener)?
