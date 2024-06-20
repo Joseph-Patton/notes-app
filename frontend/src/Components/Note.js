ced3b2db34a36ae6910b0213f2c632314c330861
Joseph Patton <josephpatton@outlook.com>
2024-06-20 12:33:12 +0100

    feat: use mui components in NoteList and Note

--- frontend/src/Components/Note.js
+++ frontend/src/Components/Note.js
@@ -1,19 +1,49 @@
 import { React } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { useStyles } from "../styles/NoteStyle";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import { grey } from "@mui/material/colors";
+import Typography from "@mui/material/Typography";
+
 function Note({ id, content, title, tag, deleteNote }) {
+  const classes = useStyles();
   return (
-    <div className="note-preview box">
-      <div className="title overflow">{title}</div>
-      <div className="text">{content}</div>
-      <div className="footer" style={{ justifyContent: "flex-end" }}>
-        <div className="tag">{tag}</div>
+    <Card className={classes.root} style={{ backgroundColor: grey }}>
+      <CardContent>
+        {/* <Checkbox
+          checked={completed}
+          onChange={toggleStatus}
+          className={classes.iconBtn}
+          style={{ left: ".2em" }}
+        /> */}
+        {/* 
+        <IconButton
+          aria-label="edit"
+          className={classes.iconBtn}
+          style={{ right: "1.8em" }}
+          onClick={() => onEdit(note)}
+        >
+          <EditIcon />
+        </IconButton> */}
         <DeleteForeverOutlinedIcon
-          className="delete"
           onClick={() => deleteNote(id)}
           aria-hidden="true"
         ></DeleteForeverOutlinedIcon>
-      </div>
-    </div>
+        <Typography className={classes.title} gutterBottom>
+          {title}
+        </Typography>
+
+        <Typography variant="body2" component="p" className={content}>
+          {content}
+        </Typography>
+
+        <Typography variant="body1" component="div" className={tag}>
+          Placeholer Date
+          {/* {DateFormatter(note.date)} */}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 export default Note;
--- frontend/src/Components/NoteList.js
+++ frontend/src/Components/NoteList.js
@@ -1,25 +1,27 @@
 import React from "react";
 import Note from "./Note.js";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+//import NoNotesIllustration from "Components/NoNotesIllustration";
 
 function NoteList({ notes, deleteNote }) {
   return (
-    // <div className="note-list">
-    //   {notes.map((note) => (
-    //     <NotePreview title={note.title} text={note.text} />
-    //   ))}
-    // </div>
-    <div className="note-list">
-      {notes.map((note) => (
-        <Note
-          key={note.id}
-          id={note.id}
-          title={note.title}
-          content={note.content}
-          tag={note.tag}
-          deleteNote={deleteNote}
-        />
-      ))}
-    </div>
+    <Grid container spacing={2}>
+      {notes.length === 0 ? (
+        <Typography>No Notes</Typography>
+      ) : (
+        notes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            content={note.content}
+            tag={note.tag}
+            deleteNote={deleteNote}
+          />
+        ))
+      )}
+    </Grid>
   );
 }
 export default NoteList;
